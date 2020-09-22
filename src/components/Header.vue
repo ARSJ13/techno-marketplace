5139855e82a3ea190b637b27b2fd98959e0f4267
Avivaldo Ribeiro <avivaldo.ribeiro@gmail.com>
2020-09-22 19:05:15 -0300

    create modal

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <header class="header">
+    <img class="logo" src="../assets/techno.svg" alt="Techno">
+    <div class="carrinho-menu">{{ count }} |</div>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: "Header",
+  props: ['count']
+}
+</script>
+
+<style scoped>
+  .header{
+    display: flex;
+    justify-content: space-between;
+    max-width: 900px;
+    margin: 0 auto;
+    padding: 20px 0;
+  }
+  .logo{
+    width: 5rem;
+  }
+  .carrinho-menu::after{
+    content: "";
+    display: inline-block;
+    background: url("../assets/carrinho.svg");
+    width: 25px;
+    height: 25px;
+    margin-left: 10px;
+  }
+  .carrinho-menu{
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    font-family: Arial, Helvetica, sans-serif;
+  }
+</style>
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,32 @@
 <template>    
   <div>
-    <section class="modal" v-if="produto">
+    <Header :count="count"></Header>
+      <div>{{ carrinho }}</div>
+    <section class="modal" v-if="produto" @click="closeModal">
       <div class="modal-container">
-        <img :src="produto.img" :alt="produto.nome">
+        <div class="modal-img">
+          <img :src="produto.img" :alt="produto.nome">
+        </div>
+        <div class="modal-body">
+          <button @click="produto = false" class="modal-button-close">X</button>
+          <span class="modal-price">{{ produto.preco | precoReal }}</span>
+          <h2 class="modal-title">{{ produto.nome }}</h2>
+          <p class="modal-description">{{ produto.descricao }}</p>
+          <button v-if="produto.estoque > 0" class="modal-btn" @click="adicionarItem">Adicionar Item</button>
+          <button v-else class="modal-btn esgotado" disabled>Estoque esgotado!</button>
+        </div>
+        <div class="modal-review">
+          <h2 class="modal-review-subtitle">Avaliações</h2>
+          <ul>
+            <li v-for="(review, id) in produto.avaliacoes" :key="id" class="review-item">
+              <p class="review-description">{{ review.descricao }}</p>
+              <p class="review-user">{{ review.nome }} | {{ review.estrelas }} estrelas</p>
+            </li>
+          </ul>
+        </div>
       </div>
-      {{produto}}
     </section>
+
     <section class="produtos">
       <div v-for="(item, index) in produtos" @click="fetchModal(item.id)" :key="index" class="produto">
         <img :src="item.img" :alt="item.name" class="produto-img">
@@ -19,12 +40,20 @@
 </template>
 
 <script>
+
+import Header from './Header'
+
 export default {
   name: "Home",
+  components:{
+    Header
+  },
   data(){
     return{
       produtos: [],
-      produto: ""
+      produto: "",
+      count: 0,
+      carrinho: []
     }
   },
   filters:{
@@ -46,6 +75,19 @@ export default {
       .then( res =>{
         this.produto = res;
       })
+      window.scrollTo({
+        top: 0
+      })
+    },
+    closeModal({target, currentTarget}){
+      if (target === currentTarget) {
+        this.produto = false;
+      }
+    },
+    adicionarItem(){
+      this.count++;
+      this.produto.estoque--;
+      this.carrinho.push(this.produto.nome);
     }
   },
   created(){
@@ -55,6 +97,120 @@ export default {
 </script>
 
 <style scoped>
+  .modal::before{
+    content: "";
+    position: fixed;
+    top: 0px;
+    left: 0px;
+    width: 100%;
+    height: 100vh;
+    background-color: rgba(0, 0, 0, 0.5);
+  }
+  .modal{
+    position: absolute;
+    top: 0px;
+    left: 0px;
+    padding: 80px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  .modal-container{
+    position: relative;
+    background: linear-gradient(to right, transparent 250px, #fff 250px);
+    z-index: 1;
+    display: grid;
+    align-items: end;
+    gap: 2rem;
+    padding: 50px 50px 50px 0px;
+    animation: fadeIn 300ms ease-out;
+  }
+
+  @keyframes fadeIn{
+    from{
+      opacity: 0;
+      transform: translate3d(50px, 0, 0);
+    }
+    to{
+      opacity: 1;
+      transform: translate3d(0px, 0, 0);
+    }
+  }
+
+  .modal-button-close{
+    border-radius: 50%;
+    border: 2px solid #000;
+    width: 40px;
+    height: 40px;
+    position: absolute;
+    top: -10px;
+    right: -10px;
+    font-size: 1rem;
+    background: #fff;
+    font-family: Arial, Helvetica, sans-serif;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.7);
+  }
+  .modal-button-close:hover{
+    background: #000;
+    color: #fff;
+    cursor: pointer;
+    outline: none;
+    transition: ease-in-out 200ms;
+  }
+  .modal-img{
+    grid-column: 1;
+    box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.1), 0px 4px 10px rgba(0, 0, 0, 0.2);
+  }
+  .modal-img>img{
+    max-width: 300px;
+    display: block;
+  }
+  .modal-body{
+    max-width: 600px;
+    grid-column: 2;
+  }
+  .modal-title{
+    font-size: 3rem;
+  }
+  .modal-btn{
+    margin-top: 80px;
+    background: #000;
+    cursor: pointer;
+    color: #fff;
+    border: none;
+    font-size: 1rem;
+    padding: 0.8rem 1.5rem;
+  }
+  .modal-btn:hover{
+    background: rgba(0, 0, 0, 0.9);
+  }
+  .modal-btn:active{
+    background: #808080;
+  }
+  .esgotado{
+    opacity: 0.6;
+  }
+  .esgotado:hover{
+    background: #000;
+  }
+  .modal-review{
+    grid-column: 2;
+  }
+  .modal-review-subtitle{
+    font-size: 1.75rem;
+  }
+  .review-item{
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+    padding-bottom: 20px;
+  }
+  .review-description{
+    margin: 20px 0px 5px 0px;
+    color: rgba(0, 0, 0, 0.7);
+  }
+  .review-user{
+    font-weight: bold;
+  }
+
   .produtos{
     max-width: 900px;
     margin: 100px auto 0 auto;
